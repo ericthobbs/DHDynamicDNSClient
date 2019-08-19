2d9d62b11ab4029c678b4847ceabf49ef4e6a472
Eric Hobbs <eric@badpointer.net>
2019-08-18 18:09:19 -0700

    Added external_ip script to repo.

---
+++ DnsClientServiceAgent/Scripts/external_ip.php
@@ -0,0 +1,6 @@
+﻿<?php
+header('Content-Type: text/xml');
+print '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+echo("<!-- For use with DynamicDNS Agent Service -->");
+echo("<ip><address>".$_SERVER["REMOTE_ADDR"]."</address></ip>");
+?>
